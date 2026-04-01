9e6eb7434db78e578f0053fa4e0f42ffab7f28ab
Adamjc987 <adamjackcross05@gmail.com>
2026-04-01 09:29:18 +0100

    Update Azure script powershell non-compliant users.ps1

--- Azure script powershell non-compliant users.ps1
+++ Azure script powershell non-compliant users.ps1
@@ -1,32 +1,32 @@
-#Requires -Modules Microsoft.Graph.Authentication, Microsoft.Graph.Users, Microsoft.Graph.Reports
- 
+#Requires -Modules Microsoft.Graph.Authentication, Microsoft.Graph.Users
+
 # Connect
-Connect-MgGraph -Scopes "User.Read.All", "AuditLog.Read.All", "Reports.Read.All" -NoWelcome
- 
-# Fetch all users
-$users = Get-MgUser -All -Property "Id,DisplayName,UserPrincipalName,AccountEnabled,AssignedLicenses,SignInActivity,PasswordPolicies"
- 
-# Fetch MFA data
+Connect-MgGraph -Scopes "User.Read.All", "UserAuthenticationMethod.Read.All" -NoWelcome
+
+# Fetch all users (no Premium properties)
+$users = Get-MgUser -All -Property "Id,DisplayName,UserPrincipalName,AccountEnabled,AssignedLicenses,PasswordPolicies"
+
+# Fetch MFA data per-user
 $mfaData = @{}
-try {
-    Get-MgReportAuthenticationMethodUserRegistrationDetail -All | ForEach-Object { $mfaData[$_.Id] = $_ }
-} catch {}
- 
-$staleDate = (Get-Date).AddDays(-90)
+foreach ($user in $users) {
+    try {
+        $methods = Get-MgUserAuthenticationMethod -UserId $user.Id
+        $mfaData[$user.Id] = $methods.Count -gt 1
+    } catch {
+        $mfaData[$user.Id] = $false
+    }
+}
+
 $nonCompliant = [System.Collections.Generic.List[PSCustomObject]]::new()
- 
+
 foreach ($user in $users) {
     $issues = [System.Collections.Generic.List[string]]::new()
- 
-    if (-not $user.AccountEnabled)                                                        { $issues.Add("Sign-in disabled") }
-    if ($user.AssignedLicenses.Count -eq 0)                                              { $issues.Add("No licence") }
-    if ($user.PasswordPolicies -match "DisablePasswordExpiration")                       { $issues.Add("Password never expires") }
-    if ($mfaData.ContainsKey($user.Id) -and -not $mfaData[$user.Id].IsMfaRegistered)    { $issues.Add("MFA not registered") }
- 
-    $lastSignIn = $user.SignInActivity?.LastSignInDateTime
-    if ($lastSignIn -and [datetime]$lastSignIn -lt $staleDate)                           { $issues.Add("Stale") }
-    elseif (-not $lastSignIn)                                                            { $issues.Add("Never signed in") }
- 
+
+    if (-not $user.AccountEnabled)                                        { $issues.Add("Sign-in disabled") }
+    if ($user.AssignedLicenses.Count -eq 0)                              { $issues.Add("No licence") }
+    if ($user.PasswordPolicies -match "DisablePasswordExpiration")        { $issues.Add("Password never expires") }
+    if ($mfaData.ContainsKey($user.Id) -and -not $mfaData[$user.Id])     { $issues.Add("MFA not registered") }
+
     if ($issues.Count -gt 0) {
         $nonCompliant.Add([PSCustomObject]@{
             Name   = $user.DisplayName
@@ -35,7 +35,10 @@ foreach ($user in $users) {
         })
     }
 }
- 
-$nonCompliant
- 
+
+# Save and auto-open results
+$nonCompliant | Export-Csv -Path "$HOME/non-compliant-users.csv" -NoTypeInformation
+Write-Host "Found $($nonCompliant.Count) non-compliant users. Opening results..." -ForegroundColor Green
+code "$HOME/non-compliant-users.csv"
+
 Disconnect-MgGraph | Out-Null
